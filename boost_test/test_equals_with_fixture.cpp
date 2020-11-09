6f3de9b8e802953df4f0a17dc7a755263ce15158
Michael J. Decker, Ph.D <mdecke@bgsu.edu>
2020-11-08 21:30:19 -0500

    Adjust fixtures

--- boost_test/test_equals_with_fixture.cpp
+++ boost_test/test_equals_with_fixture.cpp
@@ -1,18 +1,46 @@
-#define BOOST_TEST_MODULE fixture
+/**
+ * @file test_equals_with_fixutre.cpp
+ *
+ * Illustrate boost test - fixtures.  Only struct option of fixtures is shown.
+ *
+ * @author Michael J. Decker, Ph.D. <mdecke@bgsu.edu>
+ */
+
+// defines file as test driver with name "test equals"
+#define BOOST_TEST_MODULE test equals
+
+// include Boost.Test
 #include <boost/test/included/unit_test.hpp>
+
+// include object-under-test
 #include "complex.hpp"
 
-struct fixture
-{
+/* fixture struct - name is your choice
+ * declared data members are accessible in test cases
+ *
+ * Used for non-trivial setup and cleanup
+ * 
+ * constructor followed by setup (optional) is called before execution of test case
+ * teardown (optional) followed by destructor is called after each test case is executed
+ */
+struct fixture {
     std::string s;
-    fixture(std::string s = "fixture") : s(s)
-	{ BOOST_TEST_MESSAGE("ctor " << s); }
-    void setup()
-	{ BOOST_TEST_MESSAGE("optional setup " << s); }
-    void teardown()
-	{ BOOST_TEST_MESSAGE("optional teardown " << s); }
-    ~fixture()
-    { BOOST_TEST_MESSAGE("dtor " << s); }
+
+    fixture(std::string s = "fixture") : s(s) { 
+        BOOST_TEST_MESSAGE("ctor " << s);
+    }
+
+    void setup() {
+        BOOST_TEST_MESSAGE("optional setup " << s);
+    }
+
+    void teardown()	{
+        BOOST_TEST_MESSAGE("optional teardown " << s);
+    }
+
+    ~fixture() {
+        BOOST_TEST_MESSAGE("dtor " << s);
+    }
 };
 
 void setup() { BOOST_TEST_MESSAGE("free setup"); }
@@ -29,23 +57,30 @@ void teardown() { BOOST_TEST_MESSAGE("free teardown"); }
 	- fixture does not call setup/teardown UNLESS compiled with >= C++11
 */
 
-// suite level
-BOOST_AUTO_TEST_SUITE(equals_suite)
-// BOOST_AUTO_TEST_SUITE(equals_suite, *boost::unit_test::fixture<fixture>(std::string("FX")))
-// BOOST_FIXTURE_TEST_SUITE(equals_suite, fixture)
+/*
+ * can attach a fixture to a suite
+ * Before each test, constructor/setup will be called
+ * After each test, teardown/destructor will be called
+ */
+BOOST_FIXTURE_TEST_SUITE(equals_suite, fixture)
 
 
 BOOST_AUTO_TEST_CASE(equals_simple_1) {
-    BOOST_TEST_MESSAGE("test 1");
+
+    // can access fixture class's data members in test
+    BOOST_TEST_MESSAGE("test 1 " + s);
     complex zero_one(0, 0);
     complex zero_two(0, 0);
    
     BOOST_TEST(zero_one == zero_two);
 }
 
-// case level
-BOOST_FIXTURE_TEST_CASE(equals_simple_2, fixture) {
-//BOOST_AUTO_TEST_CASE(equals_simple_2) {
+BOOST_AUTO_TEST_CASE(equals_simple_2) {
+
+// can attach fixture only to specific test cases
+//BOOST_FIXTURE_TEST_CASE(equals_simple_2, fixture) {
+
+    // can access fixture class data members when BOOST_FIXTURE_TEST_CASE as well
     BOOST_TEST_MESSAGE("test 2 " + s);
     complex num_one(10, 1);
     complex num_two(10, 1);
@@ -53,27 +88,11 @@ BOOST_FIXTURE_TEST_CASE(equals_simple_2, fixture) {
     BOOST_TEST(num_one == num_two);
 }
 
-// case level free functions
-BOOST_AUTO_TEST_CASE(equals_not_1, *boost::unit_test::fixture(&setup, &teardown)) {
+BOOST_AUTO_TEST_CASE(equals_not) {
+
     BOOST_TEST_MESSAGE("test 3");
     complex num_one(11, 1);
     complex num_two(11, 2);
-   
-    BOOST_TEST(!(num_one == num_two));
-}
-
-BOOST_AUTO_TEST_CASE(equals_not_2) {
-    BOOST_TEST_MESSAGE("test 4");    
-    complex num_one(11, 1);
-    complex num_two(12, 1);
-   
-    BOOST_TEST(!(num_one == num_two));
-}
-
-BOOST_AUTO_TEST_CASE(equals_not_3) {
-    
-    complex num_one(11, 1);
-    complex num_two(12, 2);
    
     BOOST_TEST(!(num_one == num_two));
 }
